bab15593a27851a429bdf16100f869eedc8afc77
hoangthach1402 <vuhoangthach1402@gmail.com>
2025-06-19 00:06:50 +0700

    Fix deploy: use pm2 restart instead of reload to avoid cache issues

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,15 +4,14 @@ module.exports = {
       name: 'hello-vps',
       script: 'dist/main.js',
     },
-  ],
-  deploy: {
+  ],  deploy: {
     production: {
       user: 'root',
       host: '207.180.251.81',
       ref: 'origin/main',
       repo: 'git@github.com:hoangthach1402/helloNestVps.git',
       path: '/root/hello-vps',
-      'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'post-deploy': 'npm install && npm run build && pm2 restart ecosystem.config.js --env production',
     },
   },
 };
