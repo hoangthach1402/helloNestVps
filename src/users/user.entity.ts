4d6608a410028fb37887449e105efa7cc2a62956
hoangthach1402 <hoangthach1402@gmail.com>
2025-06-21 11:10:07 +0700

    fix type

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -19,11 +19,10 @@ export class User {
   firstName: string;
 
   @Column({ nullable: true })
-  lastName: string;
-  @Column({ default: true })
+  lastName: string;  @Column({ default: true })
   isActive: boolean;
 
-  @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   createdAt: Date;
 
   @ManyToOne(() => Role, role => role.users)
